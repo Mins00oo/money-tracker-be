244b5bc0522854465904ad0159ad314a42cb5179
강민수 <alstn7223@gmail.com>
2024-10-11 18:21:59 +0900

    [2024/10/11] 초기 프로젝트 업로드

---
+++ src/test/java/com/kms/moneytrackerbe/MoneyTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kms.moneytrackerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
